47e02399572611d048f6ff024a5ecee585439c61
Ranai Srivastav <ranaisrivastav@gmail.com>
2020-07-11 13:29:31 -0500

    challenge 1 started

--- Challenge1.py
+++ Challenge1.py
@@ -1,1 +1,7 @@
 print("Welcome to Mathinator")
+input1 = int(input("Please enter a number"))
+symbol = input("please enter the symbol of the operation you want to do")
+input2 = int(input("Please enter the second number"))
+
+if symbol == "+":
+    pass
